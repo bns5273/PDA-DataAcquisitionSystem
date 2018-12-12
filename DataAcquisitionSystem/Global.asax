c1a5ca57832e5f9765154aeddcfe15ecc64de6c7
Brett Nicholas Spangler <bns5273@win.psu.edu>
2018-12-12 12:02:54 -0500

    original

---
+++ DataAcquisitionSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DataAcquisitionSystem.MvcApplication" Language="C#" %>
---
+++ DataAcquisitionSystem/Models/CipDataCollection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataAcquisitionSystem.Models
+{
+    public class CipDataCollection
+    {
+        public int ID { get; set; }
+        public DateTime DateTransacted { get; set; }
+        public DateTime DateCollected { get; set; }
+        public DateTime DateCreated { get; set; }
+        public double Fit1FlowRate { get; set; }
+        public double Lt1Gallons { get; set; }
+        public double Lt2Gallons { get; set; }
+        public double Pt1Pressure { get; set; }
+        public double Pt2Pressure { get; set; }
+        public double P1Speed { get; set; }
+        public double P2Speed { get; set; }
+        public double Tt1Temperature { get; set; }
+        public double Tt2Temperature { get; set; }
+        public double Tt3Temperature { get; set; }
+    }
+}
